da66fce41d3b085b1d669776ac9dba9dd82ef17c
asimenstad <a-nna-s@hotmail.com>
2022-04-29 00:53:22 +0200

    Create nav and footer

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+const menuButton = document.querySelector(".menu-button");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const menu = document.querySelector("nav");
+
+closeMenu.style.display = "none";
+
+function toggleMenu() {
+  menu.classList.toggle("open");
+  if (menu.classList.contains("open")) {
+    openMenu.style.display = "none";
+    closeMenu.style.display = "block";
+  } else {
+    openMenu.style.display = "block";
+    closeMenu.style.display = "none";
+  }
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleDropdown() {}
