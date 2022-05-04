b3dabf266c3f6b61743cf63a7dfdd05fed19452c
asimenstad <a-nna-s@hotmail.com>
2022-05-04 22:02:01 +0200

    Fix carousel

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const recipesUrl =
   "https://annais.cool/projects/project-exam-api/wp-json/wp/v2/recipes?acf_format=standard&per_page=100&orderby=date";
-const newRecipes = document.querySelector(".recipes-container");
+const newRecipes = document.querySelector(".new-recipes-container");
+const previousSlide = document.querySelector(".previous-slide");
+const nextSlide = document.querySelector(".next-slide");
 
 async function fetchAPI() {
   try {
@@ -15,7 +17,7 @@ fetchAPI();
 
 function createCarousel(recipes) {
   for (let i = 0; i < recipes.length; i++) {
-    if (i === 9) {
+    if (i === 3) {
       break;
     }
 
@@ -24,5 +26,17 @@ function createCarousel(recipes) {
       <h4 class="recipe-category">${recipes[i].acf.category}</h4>
           <img src="${recipes[i].acf.image}" alt"" class="recipe-image">
               </div></a>`;
+
+    const allSlides = document.querySelectorAll(".recipe");
+    const slide = document.querySelector(".recipe");
+    const slideWidth = allSlides[0].getBoundingClientRect().width + 10;
+    nextSlide.addEventListener("click", moveSlide);
+
+    function moveSlide() {
+      console.log("moved");
+      for (i = 0; i < allSlides.length; i++) {
+        allSlides[i].style.transform = `translateX(-${slideWidth}px)`;
+      }
+    }
   }
 }
