040bb091d78f1603a1f698ec20ad6684af5e7f42
asimenstad <a-nna-s@hotmail.com>
2022-05-05 16:37:12 +0200

    Fix recipes.js

--- js/recipes.js
+++ js/recipes.js
@@ -1,6 +1,14 @@
 const recipesUrl =
   "https://annais.cool/projects/project-exam-api/wp-json/wp/v2/recipes?acf_format=standard&per_page=100&orderby=date";
 const recipesContainer = document.querySelector(".recipes-container");
+
+const categoryAll = document.querySelector(".all");
+const categoryBreakfast = document.querySelector(".breakfast");
+const categoryLunch = document.querySelector(".lunch");
+const categoryDinner = document.querySelector(".dinner");
+const categoryDessert = document.querySelector(".dessert");
+const categorySide = document.querySelector(".side");
+
 const moreRecipes = document.querySelector(".more-recipes");
 
 let recipes = [];
@@ -16,6 +24,7 @@ async function fetchAPI() {
 }
 fetchAPI();
 
+/// Add all recipes
 function addRecipes(recipes) {
   for (let i = 0; i < recipes.length; i++) {
     if (i === 10) {
@@ -24,6 +33,8 @@ function addRecipes(recipes) {
 
     if (recipes.length > 10) {
       moreRecipes.style.display = "block";
+    } else {
+      moreRecipes.style.display = "none";
     }
     recipesContainer.innerHTML += `<a href="#"><div class="recipe">
           <h3 class="recipe-title">${recipes[i].acf.title}</h3>
@@ -33,6 +44,33 @@ function addRecipes(recipes) {
   }
 }
 
+/// Add categories
+categoryBreakfast.addEventListener("click", addBreakfast);
+
+function addBreakfast() {
+  recipesContainer.innerHTML = "";
+  for (let i = 0; i < recipes.length; i++) {
+    categoryAll.classList.remove("active");
+    categoryBreakfast.classList.add("active");
+
+    if (recipesContainer.childElementCount > 10) {
+      moreRecipes.style.display = "block";
+    } else {
+      moreRecipes.style.display = "none";
+    }
+
+    if (recipes[i].acf.category === "breakfast") {
+      recipesContainer.innerHTML += `<a href="#"><div class="recipe">
+        <h3 class="recipe-title">${recipes[i].acf.title}</h3>
+        <h4 class="recipe-category">${recipes[i].acf.category}</h4>
+            <img src="${recipes[i].acf.image}" alt"" class="recipe-image">
+                </div></a>`;
+      console.log((recipes[i].acf.category === "breakfast").length);
+    }
+  }
+}
+
+/// View more recipes
 moreRecipes.addEventListener("click", loadMore);
 
 function loadMore() {
