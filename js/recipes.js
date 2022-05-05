fc79575945a16c4fa5e46d6046efd6eefd760ec1
asimenstad <a-nna-s@hotmail.com>
2022-05-05 11:17:07 +0200

    Fix responsiveness

--- js/recipes.js
+++ js/recipes.js
@@ -3,10 +3,12 @@ const recipesUrl =
 const recipesContainer = document.querySelector(".recipes-container");
 const moreRecipes = document.querySelector(".more-recipes");
 
+let json = [];
+
 async function fetchAPI() {
   try {
     const response = await fetch(recipesUrl);
-    const json = await response.json();
+    json = await response.json();
     addRecipes(json);
   } catch (error) {
     console.log(error);
@@ -19,10 +21,27 @@ function addRecipes(recipes) {
     if (i === 10) {
       break;
     }
+
     recipesContainer.innerHTML += `<a href="#"><div class="recipe">
           <h3 class="recipe-title">${recipes[i].acf.title}</h3>
           <h4 class="recipe-category">${recipes[i].acf.category}</h4>
               <img src="${recipes[i].acf.image}" alt"" class="recipe-image">
                   </div></a>`;
+
+    moreRecipes.addEventListener("click", loadMore);
+
+    function loadMore() {
+      for (let i = 0; i < recipes.length; i++) {
+        if (i < 10) {
+          continue;
+        }
+
+        recipesContainer.innerHTML += `<a href="#"><div class="recipe">
+  <h3 class="recipe-title">${recipes[i].acf.title}</h3>
+  <h4 class="recipe-category">${recipes[i].acf.category}</h4>
+      <img src="${recipes[i].acf.image}" alt"" class="recipe-image">
+          </div></a>`;
+      }
+    }
   }
 }
