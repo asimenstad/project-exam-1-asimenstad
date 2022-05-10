ec880bc92585c36a5fece6b5f009b0254d093349
asimenstad <a-nna-s@hotmail.com>
2022-05-10 16:42:40 +0200

    Fixed comments

--- js/comments.js
+++ js/comments.js
@@ -1,5 +1,6 @@
 const commentForm = document.querySelector("#post-comment-form");
 const commentsContainer = document.querySelector(".comments-container");
+const commentAmount = document.querySelector(".comment-amount");
 const commentUrl = "https://annais.cool/projects/project-exam-api/wp-json/wp/v2/comments";
 const specificCommentsUrl = `${commentUrl}?post=${id}`;
 
@@ -28,7 +29,6 @@ function addComment(e) {
       if (response.ok === true) {
       }
       commentForm.reset();
-
       return response.json();
     })
     .then((object) => {})
@@ -50,6 +50,7 @@ async function fetchComments() {
 fetchComments();
 
 function displayComments(comments) {
+  commentAmount.innerHTML = `(${comments.length})`;
   for (let i = 0; i < comments.length; i++) {
     commentsContainer.innerHTML += `<div class="comment-specific"><h3><span class="comment-author">${comments[i].author_name}</span> says</h3><p class="comment-date">${comments[i].date}</p>${comments[i].content.rendered}</div><hr>`;
   }
