62f257c9e14c5c3c3c9a12c73906efddd9ca1fd5
asimenstad <a-nna-s@hotmail.com>
2022-05-10 16:19:05 +0200

    Add and display comments

---
+++ js/comments.js
@@ -0,0 +1,56 @@
+const commentForm = document.querySelector("#post-comment-form");
+const commentsContainer = document.querySelector(".comments-container");
+const commentUrl = "https://annais.cool/projects/project-exam-api/wp-json/wp/v2/comments";
+const specificCommentsUrl = `${commentUrl}?post=${id}`;
+
+function addComment(e) {
+  e.preventDefault();
+
+  const [commentId, commentName, commentEmail, comment] = e.target.elements;
+
+  commentId.value = id;
+
+  const data = JSON.stringify({
+    post: commentId.value,
+    author_name: commentName.value,
+    author_email: commentEmail.value,
+    content: comment.value,
+  });
+
+  fetch(commentUrl, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data,
+  })
+    .then((response) => {
+      if (response.ok === true) {
+      }
+      commentForm.reset();
+
+      return response.json();
+    })
+    .then((object) => {})
+    .catch((error) => console.log("Error:", error));
+}
+
+commentForm.addEventListener("submit", addComment);
+
+async function fetchComments() {
+  try {
+    const response = await fetch(specificCommentsUrl);
+    const comments = await response.json();
+    console.log(comments);
+    displayComments(comments);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+fetchComments();
+
+function displayComments(comments) {
+  for (let i = 0; i < comments.length; i++) {
+    commentsContainer.innerHTML += `<div class="comment-specific"><h3><span class="comment-author">${comments[i].author_name}</span> says</h3><p class="comment-date">${comments[i].date}</p>${comments[i].content.rendered}</div><hr>`;
+  }
+}
