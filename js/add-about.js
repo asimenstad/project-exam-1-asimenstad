f454ff1bd5ea43d37d5efc1b489652877a3c3be4
asimenstad <a-nna-s@hotmail.com>
2022-05-13 00:35:13 +0200

    Fixed about.js

--- js/add-about.js
+++ js/add-about.js
@@ -1,16 +1,22 @@
-aboutUrl = "https://annais.cool/projects/project-exam-api/wp-json/wp/v2/about?acf_format=standard";
-aboutTextContainer = document.querySelector(".about-text");
-aboutImageContainer = document.querySelector(".about-image");
+const aboutUrl = "https://annais.cool/projects/project-exam-api/wp-json/wp/v2/about?acf_format=standard";
+const aboutTextContainer = document.querySelector(".about-text");
+const aboutImageContainer = document.querySelector(".about-image");
+const loader = document.querySelector(".loader");
 
 async function fetchAbout() {
   try {
     const response = await fetch(aboutUrl);
     const json = await response.json();
-    console.log(json);
-    aboutTextContainer.innerHTML = `<h2>${json[0].title.rendered}</h2><p>${json[0].content.rendered}</p>`;
-    aboutImageContainer.innerHTML = `<img src="${json[0].acf.image}" alt="">`;
+    createAboutHtml(json);
   } catch (error) {
     console.log("error");
   }
 }
 fetchAbout();
+
+function createAboutHtml(about) {
+  loader.style.display = "none";
+  console.log(about);
+  aboutTextContainer.innerHTML = `<h2>${about[0].title.rendered}</h2><p>${about[0].content.rendered}</p>`;
+  aboutImageContainer.innerHTML = `<img src="${about[0].acf.image}" alt="${about[0].acf.alt}">`;
+}
